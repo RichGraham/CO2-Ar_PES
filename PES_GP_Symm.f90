054997cf9fb1fb4e7c8de630821edc7ea8592132
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-10-09 20:38:59 +0100

    First working full version

--- PES_GP_Symm.f90
+++ PES_GP_Symm.f90
@@ -1,8 +1,8 @@
 !!CO2-Ar PES
 !!Distances in Angstrom and energies in Hartrees
 module PES_details
-  double precision :: gpRMax = 0.0  
-  double precision :: gpRMin = -100.0  
+  double precision :: gpRMax = 9.0  
+  double precision :: gpRMin = 2.0  
   double precision :: lCO = 1.1632
   double precision :: AngToBohr =  1.8897259885789
   interface PES_GP 
@@ -42,14 +42,14 @@ subroutine fixedAngleSlice()
   double precision  r, beta1, e, e_GP, asymp, PES
     
   itot=500
-  beta1 =  90   /180.0*3.14159265359
+  beta1 =  0   /180.0*3.14159265359
 
   open (unit=15, file="PES_Out.dat ", status='replace')
   
   do i=0, itot
 
      ! specify centre-to-centre separation
-     r = (  2.4 + 15.0*i/(1.0*itot) ) 
+     r = (  0.5 + 15.0*i/(1.0*itot) ) 
 
      call computeDistances(r,beta1,rab)
      
@@ -118,11 +118,11 @@ subroutine load_GP_Data
   write (filename, '( "TrainingData/HyperParams_Symm", I3.3, ".dat" )' )  nTraining
   open (unit = 7, file = filename)
   !Only need to read some as others are tied.
-  read (7,*) lScale(1), lScale(2), expVar1, expVar2,NuggVar, gpEmax
+  read (7,*) lScale(2), lScale(1), expVar1, expVar2,NuggVar, gpEmax
   !Copy over the tied values
   lScale(3) = lScale(2)
   expVar = expVar1 * expVar2
-  print *,"HyperParams",lScale(1), lScale(2), lScale(3), expVar1, expVar2,NuggVar, gpEmax
+  !print *,"HyperParams",lScale(1), lScale(2), lScale(3), expVar1, expVar2,NuggVar, gpEmax
   close(7)
   
   !====Load alpha coefficients====
@@ -130,28 +130,27 @@ subroutine load_GP_Data
   open (unit = 7, file = filename)
   do i=1,nTraining
      read (7,*) alpha(i)
-     print *,"alpha ",i, alpha(i)
+     !print *,"alpha ",i, alpha(i)
   end do
   close(7)
 
 
   !====Load training data x values ====
-  !write (filename, '( "N", I4.4, "/xTraining.dat" )' )  nTraining
-  !open (unit = 7, file = filename)
+  write (filename, '( "TrainingData/xTraining", I3.3, ".dat" )' )  nTraining
+  open (unit = 7, file = filename)
     
-  !do i=1,nTraining
-  !   read (7,*) xTraining(1,i), xTraining(2,i), xTraining(3,i), xTraining(4,i), xTraining(5,i), xTraining(6,i)  
-  !end do
+  do i=1,nTraining
+     read (7,*) xTraining(1,i), xTraining(2,i), xTraining(3,i)
+     !print *,xTraining(1,i), xTraining(2,i), xTraining(3,i)
+  end do
   !close(7)
   
   !! Permute the training vectors
-  !xTrainingPerm = xTraining
-  !do i=1,nTraining
-  !   xTrainingPerm(3,i)=xTraining(5,i)
-  !   xTrainingPerm(5,i)=xTraining(3,i)
-  !   xTrainingPerm(4,i)=xTraining(6,i)
-  !   xTrainingPerm(6,i)=xTraining(4,i)
-  !end do
+  xTrainingPerm = xTraining
+  do i=1,nTraining
+     xTrainingPerm(2,i)=xTraining(3,i)
+     xTrainingPerm(3,i)=xTraining(2,i)
+  end do
 
 end subroutine load_GP_Data
   
@@ -187,19 +186,24 @@ function PES( rab )
   implicit none
   double precision rab(3), xStar(3), asymp
   double precision  PES
+  double precision repFactor
 
+  repFactor=1.0
   
   if( rab(1) > gpRMax  .AND.  rab(2) > gpRMax .AND.  rab(3) > gpRMax &
        ) then !!Use asymptotic function
      PES = asymp(rab)
      
-  else if (rab(1) < gpRMin  .OR.  rab(2) < gpRMin  .OR.  rab(3) < gpRMin &
+  else if (rab(1) < gpRMin/repFactor  .OR.  rab(2) < gpRMin/repFactor  .OR.  rab(3) < gpRMin/repFactor &
        ) then !! Use repulsive approximation function
-     PES=gpEmax
+     PES=gpEmax* (1.0/rab(1)**12+1.0/rab(2)**12+1.0/rab(3)**12) *gpRMin **12
      
   else !! Use the Guassian Process function
      xStar(:) = 1/rab(:)
      PES = PES_GP( xStar)
   end if
 
+  !PES=gpEmax/3.0* (1.0/rab(1)**12+1.0/rab(2)**12+1.0/rab(3)**12) *gpRMin **12
+
+  
 end function PES
